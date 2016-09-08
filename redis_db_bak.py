72dbd42e386dd4bd9cd6ce5f4dbd91396f8af224
root <root@xiuba201.(none)>
2016-09-08 18:13:53 +0800

    gengxin

--- redis_db_bak.py
+++ redis_db_bak.py
@@ -6,7 +6,12 @@ import redis
 import sys
 import datetime
 import traceback
+import paramiko
 from sendmail import send_mail
+
+
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
 
 
 to_list = ['xiu8idc@xiu8.com','xiaowen@xiu8.com'] 
@@ -28,7 +33,76 @@ def read_iplist_conf(ip_filename):
     filename.close()
     return ip_dict
 
-def redis_save(redis_ip):
+def ssh_commnd(ip,port,username,passwd):
+    save_time = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S-%f")
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip,port,username,passwd)
+
+    dump_dst = '/home/xiuba/redis_db_bak/dump.rdb_' + save_time
+    appendonly_dst = '/home/xiuba/redis_db_bak/appendonly.aof_' + save_time
+
+    dump_save = 'cp /var/lib/redis/dump.rdb ' + dump_dst 
+    appendonly_save = 'cp /var/lib/redis/appendonly.aof ' + appendonly_dst
+
+    stdin, stdout, stderr = ssh.exec_command(dump_save)
+    dump_status = 'succeed'
+    for i in stderr.readlines():
+        dump_status = i
+    if dump_status == 'succeed':
+        dump_file = open('dump_filename','r')
+        dump_save_name = dump_file.readlines()[0].strip()
+        dump_file.close()
+        dump_file = open('dump_filename','w')
+        dump_file.write(dump_dst) 
+        dump_file.write("\n") 
+        dump_file.close()
+        cmd = 'rm -f ' + dump_save_name
+        stdin, stdout, stderr = ssh.exec_command(cmd)
+    else:
+        f=open(log,'a')
+        failed_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        f.write(failed_time)
+        f.write("\n")
+        f.write(dump_status)
+        f.write("----------------------------------------------------------------------------\n")
+        f.flush()
+        f.close()
+
+    stdin, stdout, stderr = ssh.exec_command(appendonly_save)
+    appendonly_status = 'succeed'
+    for i in stderr.readlines():
+       appendonly_status = i
+
+    if appendonly_status == 'succeed':
+        appendonly_file = open('appendonly_filename','r')
+        appendonly_save_name = appendonly_file.readlines()[0].strip()
+        appendonly_file.close()
+        appendonly_file = open('appendonly_filename','w')
+        appendonly_file.write(appendonly_dst)
+        appendonly_file.write("\n")
+        appendonly_file.close()
+        cmd = 'rm -f ' + appendonly_save_name
+        stdin, stdout, stderr = ssh.exec_command(cmd)
+    else:
+        f=open(log,'a')
+        failed_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        f.write(failed_time)
+        f.write("\n")
+        f.write(appendonly_status)
+        f.write("----------------------------------------------------------------------------\n")
+        f.flush()
+        f.close()
+
+    ssh.close()
+
+    result_list = []
+    result_list.append(dump_status)
+    result_list.append(appendonly_status)
+
+    return result_list
+
+def redis_connect(redis_ip):
     for i in redis_ip.keys():
         master_ip = i
         bak_ip,redis_port,redis_passwd = redis_ip[i]
@@ -37,14 +111,27 @@ def redis_save(redis_ip):
             chk_key = redis_bakup.get('redis_saof_bak')
             if chk_key == 'yes':
                 print "bak_start"            
+                dump_result,appendonly_result = ssh_commnd(bak_ip,22,'root','xiuba123')
+                if dump_result != 'succeed'  or appendonly_result != 'succeed':
+                    send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reids_bak_failed','Redis persistence backup error, please see the log') 
             else:
-                print "fasongyoujian" 
+                f=open(log,'a')
+                failed_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+                f.write(failed_time)
+                f.write("\n")
+                f.write(master_ip)
+                f.write(" The key error\n")
+                f.write("----------------------------------------------------------------------------\n")
+                f.flush()
+                f.close()
+                send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reids_bak_key_failed','Redis connection fails, please view the log') 
         except Exception,error: 
             f=open(log,'a')  
             failed_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f") 
-            f.write("----------------------------------------------------------------------------\n")
             f.write(failed_time)
             f.write("\n")
+            f.write(master_ip)
+            f.write(" ")
             traceback.print_exc(file=f)  
             f.write("----------------------------------------------------------------------------\n")
             f.flush()  
@@ -52,8 +139,7 @@ def redis_save(redis_ip):
             send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reids_bak_connect_failed','Redis connection fails, please view the log')
 
 
-
 if __name__ == "__main__":
     ipdict = read_iplist_conf('ip_list.conf')
-    redis_save(ipdict)
+    redis_connect(ipdict)
     
