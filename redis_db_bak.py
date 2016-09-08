486414fb57c3fc6b0235ea7b8aa818ab6b01f2e2
root <root@xiuba201.(none)>
2016-09-08 20:11:03 +0800

    增加日志记录函数,日志记录通过函数来写

--- redis_db_bak.py
+++ redis_db_bak.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 
+
 import redis
 import sys
 import datetime
@@ -20,9 +21,10 @@ mail_user = "xiu8idc@xiu8.com"    #用户名
 mail_pass = "FGJAxJdLQg"   #口令 
 mail_postfix = "xiu8.com"  #发件箱的后缀
 mail_addresser = "redis_xiu8idc"
-log = '/root/redis_db_bak.py/redis_bak.log'
-#mail_sub = "redis_bak"
-#mail_content = "redis_neirong"
+log = '/root/redis_db_bak/redis_bak_error.log'
+access_log = '/root/redis_db_bak/redis_bak_access.log'
+dump_filename = '/root/redis_db_bak/dump_filename'
+appendonly_filename = '/root/redis_db_bak/appendonly_filename'
 
 def read_iplist_conf(ip_filename):
     filename = open(ip_filename,'r')
@@ -50,24 +52,25 @@ def ssh_commnd(ip,port,username,passwd):
     for i in stderr.readlines():
         dump_status = i
     if dump_status == 'succeed':
-        dump_file = open('dump_filename','r')
+        dump_file = open(dump_filename,'r')
         dump_save_name = dump_file.readlines()[0].strip()
         dump_file.close()
-        dump_file = open('dump_filename','w')
+        dump_file = open(dump_filename,'w')
         dump_file.write(dump_dst) 
         dump_file.write("\n") 
         dump_file.close()
         cmd = 'rm -f ' + dump_save_name
+        cmd_status = 'succeed'
         stdin, stdout, stderr = ssh.exec_command(cmd)
+        for i in stderr.readlines():
+            cmd_status = i
+        if cmd_status != 'succeed':
+            send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reids_db_nodel','Redis backup file delete failed, please view the log')
+            log_write(log,cmd_result_status)
+        else:
+            log_write(access_log,'dump.rdb file backup success')
     else:
-        f=open(log,'a')
-        failed_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-        f.write(failed_time)
-        f.write("\n")
-        f.write(dump_status)
-        f.write("----------------------------------------------------------------------------\n")
-        f.flush()
-        f.close()
+        log_write(log,dump_status)
 
     stdin, stdout, stderr = ssh.exec_command(appendonly_save)
     appendonly_status = 'succeed'
@@ -75,24 +78,25 @@ def ssh_commnd(ip,port,username,passwd):
        appendonly_status = i
 
     if appendonly_status == 'succeed':
-        appendonly_file = open('appendonly_filename','r')
+        appendonly_file = open(appendonly_filename,'r')
         appendonly_save_name = appendonly_file.readlines()[0].strip()
         appendonly_file.close()
-        appendonly_file = open('appendonly_filename','w')
+        appendonly_file = open(appendonly_filename,'w')
         appendonly_file.write(appendonly_dst)
         appendonly_file.write("\n")
         appendonly_file.close()
         cmd = 'rm -f ' + appendonly_save_name
+        appcmd_status = 'succeed'
         stdin, stdout, stderr = ssh.exec_command(cmd)
+        for i in stderr.readlines():
+            appcmd_status = i
+        if appcmd_status != 'succeed':
+            send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reids_db_nodel','Redis backup file delete failed, please view the log')
+            log_write(log,cmd_result_status)
+        else:
+            log_write(access_log,'appendonly.aof file backup success')
     else:
-        f=open(log,'a')
-        failed_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-        f.write(failed_time)
-        f.write("\n")
-        f.write(appendonly_status)
-        f.write("----------------------------------------------------------------------------\n")
-        f.flush()
-        f.close()
+        log_write(log,appendonly_status)
 
     ssh.close()
 
@@ -110,8 +114,8 @@ def redis_connect(redis_ip):
             redis_bakup = redis.Redis(host=master_ip, port=int(redis_port), password=redis_passwd)
             chk_key = redis_bakup.get('redis_saof_bak')
             if chk_key == 'yes':
-                print "bak_start"            
                 dump_result,appendonly_result = ssh_commnd(bak_ip,22,'root','xiuba123')
+                log_write(access_log,'Redis connection is successful')
                 if dump_result != 'succeed'  or appendonly_result != 'succeed':
                     send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reids_bak_failed','Redis persistence backup error, please see the log') 
             else:
@@ -124,7 +128,7 @@ def redis_connect(redis_ip):
                 f.write("----------------------------------------------------------------------------\n")
                 f.flush()
                 f.close()
-                send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reids_bak_key_failed','Redis connection fails, please view the log') 
+                send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reids_bak_key_failed','Redis keys error, please see the log') 
         except Exception,error: 
             f=open(log,'a')  
             failed_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f") 
@@ -138,8 +142,20 @@ def redis_connect(redis_ip):
             f.close()  
             send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reids_bak_connect_failed','Redis connection fails, please view the log')
 
+def log_write(log_file,conment,ip="None_ip"):
+    f=open(log_file,'a')
+    failed_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    f.write(failed_time)
+    f.write("\n")
+    f.write(conment)
+    f.write("\n")
+    f.write("----------------------------------------------------------------------------\n")
+    f.flush()
+    f.close()
 
 if __name__ == "__main__":
-    ipdict = read_iplist_conf('ip_list.conf')
+    ip_filename = '/root/redis_db_bak/ip_list.conf'
+    f = open('redis_bak_access.log','a')
+    ipdict = read_iplist_conf(ip_filename)
     redis_connect(ipdict)
     
