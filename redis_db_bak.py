bb11d0f2ea273bec24bf12acf1e49d7f3e0b8e17
root <root@xiuba201.(none)>
2016-10-10 10:52:15 +0800

    2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update2016-10-10 redis_db_bak.py update

--- redis_db_bak.py
+++ redis_db_bak.py
@@ -25,52 +25,43 @@ log = '/root/redis_db_bak/redis_bak_error.log'
 access_log = '/root/redis_db_bak/redis_bak_access.log'
 dump_filename = '/root/redis_db_bak/dump_filename'
 appendonly_filename = '/root/redis_db_bak/appendonly_filename'
+local_ip = '192.168.1.201'
 
 def read_iplist_conf(ip_filename):
     filename = open(ip_filename,'r')
     ip_dict = {}
     for i in filename.readlines():
-        master_ip,backup_ip,port,redis_passwd,redis_src,redis_dst = i.strip().split(',')
-        ip_dict[master_ip] = backup_ip,port,redis_passwd,redis_src,redis_dst
+        redis_name,master_ip,backup_ip,port,redis_passwd,redis_src,redis_dst = i.strip().split(',')
+        ip_dict[redis_name] = master_ip,backup_ip,port,redis_passwd,redis_src,redis_dst
     filename.close()
     return ip_dict
 
-def ssh_commnd(ip,port,username,passwd,redisbak_src,redisbak_dst):
+def ssh_commnd(ip,port,username,passwd,redisbak_src,redisbak_dst,redis_port):
     save_time = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S-%f")
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(ip,port,username,passwd)
 
-    dump_dst = redisbak_dst + '/dump.rdb_' + ip + '_' + save_time
-    appendonly_dst = redisbak_dst + '/appendonly.aof_' + ip + '_' + save_time
+    dump_dst = redisbak_dst + '/dump.rdb_' + ip + '_' + str(redis_port) + '_' + save_time
+    appendonly_dst = redisbak_dst + '/appendonly.aof_' + ip + '_' + str(redis_port) + '_' + save_time
 
     dump_save = 'cp ' + redisbak_src + '/dump.rdb ' + dump_dst 
     appendonly_save = 'cp ' + redisbak_src + '/appendonly.aof ' + appendonly_dst
 
     stdin, stdout, stderr = ssh.exec_command(dump_save)
     dump_status = 'succeed'
+
     for i in stderr.readlines():
         dump_status = i
+
     if dump_status == 'succeed':
-        dump_file = open(dump_filename,'r')
-        dump_save_name = dump_file.readlines()[0].strip()
-        dump_file.close()
-        dump_file = open(dump_filename,'w')
-        dump_file.write(dump_dst) 
-        dump_file.write("\n") 
-        dump_file.close()
-        cmd = 'rm -f ' + dump_save_name
-        cmd_status = 'succeed'
-        stdin, stdout, stderr = ssh.exec_command(cmd)
-        for i in stderr.readlines():
-            cmd_status = i
-        if cmd_status != 'succeed':
-            send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reids_db_nodel','Redis backup file delete failed, please view the log')
-            log_write(log,cmd_result_status)
-        else:
-            log_write(access_log,'dump.rdb file backup success')
+        success_conment = ip + '_' + redis_port + ' Redis dump.db persistent file backup successfully'
+        log_write(access_log,ip,success_conment) 
     else:
-        log_write(log,dump_status)
+        faild_conment = ip + '_' + redis_port + local_ip +' Redis dump.db persistence failed to backup file'
+        log_write(log,dump_status,faild_conment)
+        send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reid_dump_bak_fiaid',faild_conment)
+        
 
     stdin, stdout, stderr = ssh.exec_command(appendonly_save)
     appendonly_status = 'succeed'
@@ -78,25 +69,12 @@ def ssh_commnd(ip,port,username,passwd,redisbak_src,redisbak_dst):
        appendonly_status = i
 
     if appendonly_status == 'succeed':
-        appendonly_file = open(appendonly_filename,'r')
-        appendonly_save_name = appendonly_file.readlines()[0].strip()
-        appendonly_file.close()
-        appendonly_file = open(appendonly_filename,'w')
-        appendonly_file.write(appendonly_dst)
-        appendonly_file.write("\n")
-        appendonly_file.close()
-        cmd = 'rm -f ' + appendonly_save_name
-        appcmd_status = 'succeed'
-        stdin, stdout, stderr = ssh.exec_command(cmd)
-        for i in stderr.readlines():
-            appcmd_status = i
-        if appcmd_status != 'succeed':
-            send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reids_db_nodel','Redis backup file delete failed, please view the log')
-            log_write(log,cmd_result_status)
-        else:
-            log_write(access_log,'appendonly.aof file backup success')
+        success_conment = ip + '_' + redis_port + ' Redis appendonly.aof persistent file backup successfully'
+        log_write(access_log,ip,success_conment) 
     else:
-        log_write(log,appendonly_status)
+        faild_conment = ip + '_' + redis_port + local_ip + ' Redis appendonly.aof persistence failed to backup file'
+        log_write(log,appendonly_status,conment)
+        send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'Redis_appendonly_bak_fiaid',faild_conment)
 
     ssh.close()
 
@@ -108,27 +86,20 @@ def ssh_commnd(ip,port,username,passwd,redisbak_src,redisbak_dst):
 
 def redis_connect(redis_ip):
     for i in redis_ip.keys():
-        master_ip = i
-        bak_ip,redis_port,redis_passwd,redisbak_src,redisbak_dst = redis_ip[i]
+        redis_name = i
+        master_ip,bak_ip,redis_port,redis_passwd,redisbak_src,redisbak_dst = redis_ip[i]
         try: 
             redis_bakup = redis.Redis(host=master_ip, port=int(redis_port), password=redis_passwd)
             log_write(access_log,'Redis connection is successful')
             chk_key = redis_bakup.get('redis_saof_bak')
             if chk_key == 'yes':
-                dump_result,appendonly_result = ssh_commnd(bak_ip,22,'root','xiuba123',redisbak_src,redisbak_dst)
+                dump_result,appendonly_result = ssh_commnd(bak_ip,22,'root','xiuba123',redisbak_src,redisbak_dst,redis_port)
                 if dump_result != 'succeed'  or appendonly_result != 'succeed':
-                    send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reids_bak_failed','Redis persistence backup error, please see the log') 
+                    send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reids_bak_failed','192.168.1.201 Redis persistence backup error, please see the log') 
             else:
-                f=open(log,'a')
-                failed_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-                f.write(failed_time)
-                f.write("\n")
-                f.write(master_ip)
-                f.write(" The key error\n")
-                f.write("----------------------------------------------------------------------------\n")
-                f.flush()
-                f.close()
-                send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reids_bak_key_failed','Redis keys error, please see the log') 
+                conment = master_ip + '_' + redis_port + " The key error"
+                log_write(log,master_ip,conment)
+                send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reids_bak_key_failed','192.168.1.201 Redis keys error, please see the log') 
         except Exception,error: 
             f=open(log,'a')  
             failed_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f") 
@@ -142,12 +113,14 @@ def redis_connect(redis_ip):
             f.close()  
             send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'reids_bak_connect_failed','Redis connection fails, please view the log')
 
-def log_write(log_file,conment,ip="None_ip"):
+def log_write(log_file,conment,conment_2=''):
     f=open(log_file,'a')
     failed_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
     f.write(failed_time)
     f.write("\n")
     f.write(conment)
+    f.write("\n")
+    f.write(conment_2)
     f.write("\n")
     f.write("----------------------------------------------------------------------------\n")
     f.flush()
@@ -159,9 +132,8 @@ def procedure_execute():
         send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'Redis_backup_program_run','192.168.1.201 Redis backup program run') 
 
 if __name__ == "__main__":
-    procedure_execute()
     ip_filename = '/root/redis_db_bak/ip_list.conf'
-    f = open('redis_bak_access.log','a')
     ipdict = read_iplist_conf(ip_filename)
     redis_connect(ipdict)
+    procedure_execute()
     
--- sendmail.py
+++ sendmail.py
@@ -3,14 +3,20 @@
 
 import smtplib  
 from email.mime.text import MIMEText  
-#mailto_list = ['xiu8idc@xiu8.com','xiaowen@xiu8.com'] 
-#mail_host = "mail.xiu8.com"  #设置服务器
-#mail_user = "xiu8idc@xiu8.com"    #用户名
-#mail_pass = "FGJAxJdLQg"   #口令 
-#mail_postfix = "xiu8.com"  #发件箱的后缀
-#mail_addresser = "xiu8idc"
-#mail_sub = "zhuti"
-#mail_content = "neirong"
+mailto_list = ['xiu8idc@xiu8.com','xiaowen@xiu8.com'] 
+mail_host = "mail.xiu8.com"  #设置服务器
+mail_user = "xiu8idc@xiu8.com"    #用户名
+mail_pass = "FGJAxJdLQg"   #口令 
+mail_postfix = "xiu8.com"  #发件箱的后缀
+mail_addresser = "xiu8idc"
+mail_sub = "zhuti"
+mail_content = "neirong"
+
+
+def procedure_execute():
+    shijian = datetime.datetime.now().strftime("%H%M")  
+    if shijian == '1205':
+        send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,'test','192.168.1.201 test') 
   
 def send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,sub,content):  
     me=mail_addresser+"<"+mail_user+"@"+mail_postfix+">"  
@@ -29,9 +35,4 @@ def send_mail(mail_host,mail_user,mail_pass,mail_postfix,mail_addresser,to_list,
         print str(e)  
         return False  
 if __name__ == '__main__':  
-    print "enen"
-#else:
-#   send_mail(host,user,passwd,pastfix,addresser,list,sub,content)
-#      print "fasong"  
-#    else:  
-#      print "fasong"  
+   send_mail(host,user,passwd,pastfix,addresser,list,sub,content)
